18b6a58fbf39e07bb17fa291c0bf750d4382e21a
Thomas Walpole <twalpole@gmail.com>
2019-04-13 13:42:10 -0700

    use should

--- spec/ruby/core/enumerator/lazy/select_spec.rb
+++ spec/ruby/core/enumerator/lazy/select_spec.rb
@@ -13,9 +13,9 @@ describe "Enumerator::Lazy#select" do
       true
     end
 
-    expect do
+    lambda {
       enum.next
-    end.to change { eval_count }.from(0).to(1)
+    }.should change { eval_count }.from(0).to(1)
   end
 
   it "doesn't over-evaluate when peeked" do
@@ -25,10 +25,10 @@ describe "Enumerator::Lazy#select" do
       true
     end
 
-    expect do
+    lambda {
       enum.peek
       enum.peek
-    end.to change { eval_count }.from(0).to(1)
+    }.should change { eval_count }.from(0).to(1)
   end
 
   it "doesn't re-evaluate after peek" do
@@ -38,10 +38,10 @@ describe "Enumerator::Lazy#select" do
       true
     end
 
-    expect do
+    lambda {
       enum.peek
       enum.next
-    end.to change { eval_count }.from(0).to(1)
+    }.should change { eval_count }.from(0).to(1)
   end
 end
 
